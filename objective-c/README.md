736e95493b142a31b22ad2a47a4d36a2702e030b
Arcxm <Arcxm@users.noreply.github.com>
2023-08-17 00:02:51 +0200

    objective-c update code and README

--- objective-c/README.md
+++ objective-c/README.md
@@ -6,6 +6,17 @@ This is the Objective-C implementation of lolcat.
 
 ### Windows
 
+#### After Update
+
+Using [GNUstep Windows MSVC Toolchain](https://github.com/gnustep/tools-windows-msvc).
+
+```console
+$ clang-cl -I <path/to/gnustep>\include -fobjc-runtime=gnustep-2.0 -Xclang -fexceptions -Xclang -fobjc-exceptions -fblocks -DGNUSTEP -DGNUSTEP_WITH_DLL -DGNUSTEP_RUNTIME=1 -D_NONFRAGILE_ABI=1 -D_NATIVE_OBJC_EXCEPTIONS /MDd /Z7 /c main.m
+$ clang-cl main.obj gnustep-base.lib objc.lib dispatch.lib -fuse-ld=lld /MDd /Z7 -o lolcat.exe /link /LIBPATH:<path/to/gnustep>\lib
+```
+
+#### Before Update
+
 Using [GNUstep](https://github.com/gnustep/) with MinGW.
 
 ```console
--- objective-c/main.m
+++ objective-c/main.m
@@ -1,21 +1,24 @@
 #import <Foundation/Foundation.h>
 
-#import <stdio.h>
-#import <stdlib.h>
-#import <stdint.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#import <math.h>
+#include <math.h>
+
+#ifndef PI
 #define PI (acos(-1))
+#endif
 
 #define COLOR_RESET "\x1B[0m"
 
 #define FREQ 0.1
 
-@interface rgb:NSObject {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-}
+@interface RGB : NSObject
+
+@property (nonatomic, assign) uint8_t r;
+@property (nonatomic, assign) uint8_t g;
+@property (nonatomic, assign) uint8_t b;
 
 // rainbow
 - (id) initWithFrequency: (double) frequency andI: (int) i;
@@ -23,41 +26,42 @@
 
 @end
 
-@implementation rgb
+@implementation RGB
 
 - (id) initWithFrequency: (double) frequency andI: (int) i {
-    r = sin(frequency * i) * 127 + 128;
-    g = sin(frequency * i + 2 * PI / 3) * 127 + 128;
-    b = sin(frequency * i + 4 * PI / 3) * 127 + 128;
+    self.r = sin(frequency * i) * 127 + 128;
+    self.g = sin(frequency * i + 2 * PI / 3) * 127 + 128;
+    self.b = sin(frequency * i + 4 * PI / 3) * 127 + 128;
+
+    return self;
 }
 
 - (NSString*) escapeSequence {
-    return [NSString stringWithFormat:@"\x1B[38;2;%d;%d;%dm", r, g, b];
+    return [NSString stringWithFormat:@"\x1B[38;2;%d;%d;%dm", self.r, self.g, self.b];
 }
 
 @end
 
-void putc_with_rgb(char c, rgb *color) {
+void putc_with_rgb(char c, RGB *color) {
     printf("%s%c%s", [[color escapeSequence] UTF8String], c, COLOR_RESET);
 }
 
 int main(int argc, char **argv) {
-    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
     int exitCode = EXIT_SUCCESS;
 
-	// Check if at least one command line argument was provided
-    if (argc > 1) {
-        NSString *input = [NSString stringWithUTF8String: argv[1]];
+    @autoreleasepool {
+        // Check if at least one command line argument was provided
+        if (argc > 1) {
+            NSString *input = [NSString stringWithUTF8String: argv[1]];
 
-        NSUInteger sz = [input length];
-        for (NSUInteger i = 0; i < sz; ++i) {
-            putc_with_rgb([input characterAtIndex: i], [[rgb alloc] initWithFrequency: FREQ andI: i]);
+            NSUInteger sz = [input length];
+            for (NSUInteger i = 0; i < sz; ++i) {
+                putc_with_rgb([input characterAtIndex: i], [[RGB alloc] initWithFrequency: FREQ andI: i]);
+            }
+        } else {
+            exitCode = EXIT_FAILURE;
         }
-    } else {
-        exitCode = EXIT_FAILURE;
     }
 
-    [pool drain];
     return exitCode;
 }
